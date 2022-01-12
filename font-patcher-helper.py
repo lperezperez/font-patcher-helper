de564b52ef6ccca9d9fcb0569a34e6799455e6b2
Lus Pérez <lperezperez@gmail.com>
2022-01-12 16:24:58 +0100

    Update font patcher helper script

--- font-patcher-helper.py
+++ font-patcher-helper.py
@@ -1,106 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from json import load
-from itertools import islice
-from multiprocessing import cpu_count, Process
-from os import makedirs, path, system, walk
-from re import compile, match
-import sys
-from urllib import request
+from helpers import github, fonts
+from os import path
+from sys import argv, stderr
 
-FONT_PATCHER = "font-patcher"
-repo_only_url = compile(r"https:\/\/github\.com\/[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}\/[a-zA-Z0-9]+$")
-re_branch = compile("/(tree|blob)/(.+?)/")
-def download(url, output_folder = "./", full_path = True):
-	"""
-	Downloads the files and directories under url to output_folder.
+FONT_PATCHER_SCRIPT = "font-patcher" # Nerd fonts patcher script.
+TEST_FONTS_SCRIPT = "test-fonts.sh" # Test Nerd fonts script.
 
-	Parameters:
-		url (str): The URL of the subfolder to extract.
-		output_folder (str): The base path where the subfolder will be dowloaded.
-		full_path (bool): Download to full path.
-	"""
-	# Check if the URL is from a full GitHub repo. If it is, use 'git clone' to download it.
-	if match(repo_only_url, url):
-		exit("The URL {} is a full repository. Use 'git clone' to download the repository.".format(url))
-	branch = re_branch.search(url) # Extract the branch name from the URL (e.g master).
-	try:
-		response = request.urlretrieve((url[:branch.start()].replace("github.com", "api.github.com/repos", 1) + "/contents/" + url[branch.end():] + "?ref=" + branch.group(2)))
-	except KeyboardInterrupt:
-		exit("Process cancelled by user.")
-	with open(response[0], "r") as stream:
-		json = load(stream)
-		# If the json data is a file entry, download it.
-		if isinstance(json, dict) and json["type"] == "file":
-			try:
-				# Download the file.
-				request.urlretrieve(json["download_url"], path.join(output_folder, json["path"] if full_path else path.basename(json["path"])))
-				return 1
-			except KeyboardInterrupt:
-				exit("Process cancelled by user.")
-		# Get number of files to use it later for the output information.
-		total_files = len(json)
-		# For each file or directory in 
-		for file in json:
-			base_path = path.dirname(file["path"])
-			if base_path != "":
-				makedirs(path.join(output_folder, base_path), exist_ok = True)
-			if file["download_url"] is not None:
-				try:
-					# Download the file.
-					request.urlretrieve(file["download_url"], path.join(output_folder, file["path"] if full_path else path.basename(file["path"])))
-				except KeyboardInterrupt:
-					exit("Process cancelled by user.")
-			else:
-				download(file["html_url"], output_folder)
-		return total_files
-
-
-def run_patcher(font_file, output_dir):
-	"""
-	Runs the Nerd fonts patcher.
-	Arguments:
-		font_file (str): The font file path to patch.
-		output_dir (str): The output directory where the patched font will be stored.
-	"""
-	makedirs(output_dir, exist_ok = True)
-	system("./font-patcher -w -c \"{}\" -out \"{}\"".format(font_file, output_dir))
-
-# Check arguments
-if len(sys.argv) == 0:
-	exit("At least one path to a font or folder which contains fonts must be provided.")
-# Download Nerd fonts patcher (if needed)
-if not path.isfile(FONT_PATCHER) and download("https://github.com/ryanoasis/nerd-fonts/tree/master/" + FONT_PATCHER) > 0:
-	print(FONT_PATCHER + " script dowloaded.")
-# Download font glyphs (if needed)
+# Check arguments.
+if len(argv) < 2:
+	stderr.write("Usage: ./font-patcher-helper.py [font_path]...")
+	exit(1)
+# Download Nerd fonts patcher (if needed).
+if not path.isfile(FONT_PATCHER_SCRIPT) and github.download("https://github.com/ryanoasis/nerd-fonts/tree/master/" + FONT_PATCHER_SCRIPT) > 0:
+	print(FONT_PATCHER_SCRIPT + " script dowloaded.")
+# Download font glyphs (if needed).
 if not path.isdir(path.join("src", "glyphs")):
-	files = download("https://github.com/ryanoasis/nerd-fonts/tree/master/src/glyphs")
+	files = github.download("https://github.com/ryanoasis/nerd-fonts/tree/master/src/glyphs")
 	if files > 0:
-		print("{} glyphs fonts downloaded.".format(files))
-# Download Nerd fonts test (if needed)
-if not path.isfile("test-fonts.sh") and download("https://github.com/ryanoasis/nerd-fonts/tree/master/bin/scripts/test-fonts.sh", full_path = False) > 0:
-	print("test-fonts.sh script dowloaded.")
-output_path = "./patched-fonts"  # Set output path
-fonts = {}
-for font_path in sys.argv[1:]:
-	if path.isfile(font_path):
-		fonts[font_path] = path.join(output_path, path.basename(font_path))
-	elif path.isdir(font_path):
-		for root, folder_names, file_names in walk(font_path):
-			for file_name in file_names:
-				if path.splitext(file_name)[-1] in [".otf", ".ttf"]:
-					fonts[path.join(root, file_name)] = path.join(output_path, path.basename(font_path))
-	else:
-		print("The path {} is invalid".format(font_path))
-processes = []
-process_count = cpu_count()
-for source, dest in fonts.items():
-	if len(processes) == process_count:
-		for process in processes:
-			process.join()
-		processes = []
-	processes.append(Process(target=run_patcher, args = (source, dest)))
-	processes[-1].start()
-if len(processes) > 0:
-	for process in processes:
-		process.join()
+		print(f"{files} glyphs fonts downloaded.")
+# Download Nerd fonts test (if needed).
+if not path.isfile(TEST_FONTS_SCRIPT) and github.download("https://github.com/ryanoasis/nerd-fonts/tree/master/bin/scripts/" + TEST_FONTS_SCRIPT, full_path = False) > 0:
+	print(f"{TEST_FONTS_SCRIPT} script dowloaded.")
+fonts.run_in_parallel(fonts.get_font_files(argv[1:]), fonts.run_patcher, (fonts.PATCHED_FONTS_PATH,)) # Patch fonts.
+fonts.run_in_parallel(fonts.get_font_files([fonts.PATCHED_FONTS_PATH]), fonts.rename_font) # Rename fonts.
